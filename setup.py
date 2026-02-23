6a612f7c79c77657d20b1fe12df29209f77f54a4
AnushreeHazra <anushreehazra27@gmail.com>
2026-02-23 20:38:58 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Green_Vision",
+    version="0.0.1",
+    author="Anushree", 
+    author_email="anushreehazra27@gmail.com", 
+    packages=find_packages(),
+    install_requires=[]
+)
